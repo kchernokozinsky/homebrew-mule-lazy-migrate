4cd75136d33aaf671333169823da1a77d369eaa4
kyrylo_c <kyr.cherny@gmail.com>
2025-06-26 19:44:49 +0300

    Update mule-lazy-migrate to v0.1.3

--- Formula/mule-lazy-migrate.rb
+++ Formula/mule-lazy-migrate.rb
@@ -1,8 +1,8 @@
 class MuleLazyMigrate < Formula
   desc "Mule 4 migration tool"
   homepage "https://github.com/kchernokozinsky/mule-lazy-migrate"
-  url "https://github.com/kchernokozinsky/mule-lazy-migrate/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "cf0ac94c8a97eb30255ccb54f80f23ae25d0b24fcf6d283c0b03e0399b0f481d"
+  url "https://github.com/kchernokozinsky/mule-lazy-migrate/archive/refs/tags/v0.1.3.tar.gz"
+  sha256 "0d72a9d5b3da4e57fb8c8b453e5a88155f0c1570ab3eb47757b7db189540eeb4"
   license "MIT"
 
   depends_on "rust" => :build
